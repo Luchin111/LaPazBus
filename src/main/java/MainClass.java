418329652fc7d16dc3e312dc612d8a84646cd8a6
Luis <luisinmedina@gmail.com>
2019-11-01 18:54:52 -0400

    Prueba Conexion base de datos

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,3 +1,4 @@
+import bot.MainBot;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
@@ -5,5 +6,12 @@ import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class MainClass {
     public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new MainBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }

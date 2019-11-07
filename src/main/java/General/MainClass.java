0f12c72feb292038c3b0285865a163c3892da722
Carlos Cavero <54409608+ccaverotx@users.noreply.github.com>
2019-11-07 17:36:38 -0400

    Bot Funcional, aún sin jalar datos. :disappointed:

--- src/main/java/General/MainClass.java
+++ src/main/java/General/MainClass.java
@@ -1,47 +1,22 @@
 package General;
 
-import bl.MovilidadBl;
-import bot.BotBDD;
+import bot.MainBot;
+
+import org.springframework.boot.test.context.SpringBootTest;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-@SpringBootApplication
+@SpringBootTest
 public class MainClass {
-    /*
-    public static void main(String[] args) {
-          SpringApplication.run(MainClass.class, args);
-    }
-    */
-    static MovilidadBl movilidadBl;
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
-            telegramBotsApi.registerBot(new BotBDD(movilidadBl));
+            telegramBotsApi.registerBot(new MainBot());
         } catch (TelegramApiException e) {
             e.printStackTrace();
-
         }
     }
 }
-/*
-    static CustomerBl customerBl;
-    public static void main(String[] args) {
-
-        ApiContextInitializer.init();
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-        try {
-            telegramBotsApi.registerBot(new BotBDD(customerBl));
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-
-        }
-    }
-}
-*/
-
--- src/main/java/bot/BotInitializator.java
+++ src/main/java/bot/BotInitializator.java
@@ -1,9 +1,5 @@
 package bot;
 
-import bl.MovilidadBl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
@@ -13,36 +9,33 @@ import javax.annotation.PostConstruct;
 
 @Component
 public class BotInitializator {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(BotInitializator.class);
-
-    MovilidadBl movilidadBl;
-    @Autowired
-    public BotInitializator(MovilidadBl movilidadBl) {
-        this.movilidadBl = movilidadBl;
-    }
-
     public BotInitializator() {
     }
     @PostConstruct
     public void init(){
-        LOGGER.info("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
-        LOGGER.info("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
-        LOGGER.info("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
-        LOGGER.info("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
-        LOGGER.info("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
-        LOGGER.info("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
-        LOGGER.info("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
 
+        System.out.println("INICIALIZAR EL BOT");
+        System.out.println("INICIALIZAR EL BOT");
+        System.out.println("INICIALIZAR EL BOT");
+        System.out.println("INICIALIZAR EL BOT");
+        System.out.println("INICIALIZAR EL BOT");
+        System.out.println("INICIALIZAR EL BOT");
+        System.out.println("INICIALIZAR EL BOT");
+        System.out.println("INICIALIZAR EL BOT");
+        System.out.println("INICIALIZAR EL BOT");
+        System.out.println("INICIALIZAR EL BOT");
+        System.out.println("INICIALIZAR EL BOT");
+        System.out.println("INICIALIZAR EL BOT");
+        System.out.println("INICIALIZAR EL BOT");
 
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
-            telegramBotsApi.registerBot(new BotBDD(movilidadBl));
+            telegramBotsApi.registerBot(new MainBot());
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
- /**/
+
 
     }
 
